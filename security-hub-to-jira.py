f1b8f91ca92214eb1b0646626f5f2bb14817dc0b
bhoff <bruce.hoff@sagebase.org>
2022-10-01 20:44:15 -0700

    group inspector findings by resoruce, not vuln

--- security-hub-to-jira.py
+++ security-hub-to-jira.py
@@ -4,6 +4,7 @@ from atlassian import Jira
 import os
 import getopt
 import sys
+from enum import Enum
 
 # https://atlassian-python-api.readthedocs.io/
 # pip install atlassian-python-api
@@ -12,6 +13,18 @@ security_hub_label='security-hub'
 auto_generated_label='auto-generated'
 jql_request = f"labels = {security_hub_label} and labels={auto_generated_label}"
 limit = 20
+LEN=100
+
+class Finding_Group_By(Enum):
+	VULNERABILITY=1,
+	RESOURCE=2
+
+Product_To_Group_by={"Inspector": Finding_Group_By.RESOURCE}
+
+# Group by Vulnerability unless otherwise specified
+def get_group_by(product):
+	return Product_To_Group_by.get(product, Finding_Group_By.VULNERABILITY)
+
 
 def create_filter(aws_account_ids, severity_labels):
 	aws_accounts = list(map(lambda x:  {'Value': x,'Comparison': 'EQUALS'}, aws_account_ids))
@@ -28,6 +41,15 @@ def create_filter(aws_account_ids, severity_labels):
         ]
 	}
 
+def resource_descriptor(r):
+	result =  f"Type: {r['Type']}, Id: {r['Id']}"
+	if "Name" in r and r["Name"] is not None:
+		result = result + f", Name: {r['Name']}"
+	return result
+	
+def resource_name(r):
+	return r.get("Tags",{"Name":None}).get("Name", None)
+		
 # retrieve SecurityHub findings, grouping together findings having the same
 # AwsAccountId and Title and grouping the associated "resources" into a single list
 def get_sec_hub_findings(aws_account_ids, severity_labels):
@@ -50,16 +72,34 @@ def get_sec_hub_findings(aws_account_ids, severity_labels):
 			severity = f['Severity']['Label']
 			title = f['Title']
 			description = f['Description']
-			resources = [] # subselect critical metadata
-			for r in f['Resources']:
-				resources.append({"Type":r["Type"], "Id":r["Id"], "Name":r.get("Tags",{"Name":None}).get("Name", None)})
-			key = (account, title)
-			if key in findings:
-				record = findings[key]
+			product = f['ProductName']
+				
+			group_by = get_group_by(product)
+			if group_by==Finding_Group_By.VULNERABILITY:
+				key = (account, title)
+				if key in findings:
+					record = findings[key]
+				else:
+					record = {"account":account, "product":product, "title":title, "severity":severity, "description":description, "resources":[]}
+					findings[key] = record
+				resources = []
+				for resource in f['Resources']:
+					resources.append(resource_descriptor({"Type":resource["Type"], "Id":resource["Id"], "Name":resource_name(resource)}))
+				record["resources"].extend(resources)	
+			elif group_by==Finding_Group_By.RESOURCE:
+				for resource in f['Resources']:
+					name = resource_name(resource) if resource_name(resource) is not None else resource["Id"]
+					key = (account, name)
+					if key in findings:
+						record = findings[key]
+					else:
+						record = {"account":account, "product":product, "name":name, "vulnerabilities":[], "severity":severity,  "resources":set()}
+						findings[key] = record
+					record["resources"].add(resource_descriptor(resource))	
+					record["vulnerabilities"].append({"title":title, "description":description})	
 			else:
-				record = {"account":account, "title":title, "severity":severity, "description":description, "resources":[]}
-				findings[key] = record
-			record["resources"].extend(resources)
+				raise Error(f"Unexpected: {group_by}")
+
 		if 'NextToken' in response:
 			next_token = response['NextToken']
 		else:
@@ -87,15 +127,21 @@ def get_jira_sec_hub_issues(jira):
 		start=start+limit
 		return result
 		
+		
 def resources_to_text(resources):
 	result = ""
 	for r in resources:
-		result = result + f"\n\tType: {r['Type']}, Id: {r['Id']}"
-		if "Name" in r and r["Name"] is not None:
-			result = result + f", Name: {r['Name']}"
+		result = result + f"\n\t{r}"
+	return result
+		
+def vulnerabilities_to_text(vs):
+	result = ""
+	for v in vs:
+		result = result + f"\n\t{v['title']}: {v['description']}"
 	return result
 		
 ARG_HELP =  'python3 security-hub-to-jira.py --account 325565585839[,383874245509,...] --severity CRITICAL[,HIGH,...] --dryrun  --verbose'
+WARNING_HDR = "Machine generated description.  Do not alter.\n\n"
 
 def main():
 	dry_run=False
@@ -132,8 +178,15 @@ def main():
 	
 	# create or update Jira issues
 	for finding in sec_hub_findings:
-		jira_summary=f"{finding['account']} {finding['title']}"
-		description = f"Machine generated description.  Do not alter.\n\n{finding['description']}\n\nAffected resources:\n{resources_to_text(finding['resources'])}"
+		group_by = get_group_by(finding['product'])
+		if group_by==Finding_Group_By.VULNERABILITY:
+			jira_summary=f"{finding['account']} {finding['title']}"
+			description = f"{WARNING_HDR}{finding['description']}\n\nAffected resources:\n{resources_to_text(finding['resources'])}"
+		elif group_by==Finding_Group_By.RESOURCE:
+			jira_summary=f"{finding['account']} {finding['name']}"
+			description = f"{WARNING_HDR}Vulnerabilities:\n{vulnerabilities_to_text(finding['vulnerabilities'])}\n\nResources:\n{resources_to_text(finding['resources'])}"
+		else:
+			raise Error(f"Unexpected: {group_by}")
 		if jira_summary in issues:
 			issue = issues[jira_summary]
 			key = issue['key']
@@ -144,7 +197,7 @@ def main():
 				print(f"Nothing to update in **{status}** {key} {jira_summary}")
 			else:
 				if dry_run:
-					print(f"UPDATE existing issue {key} {jira_summary} {finding['description']} {resources_to_text(finding['resources']) if verbose else ''}")
+					print(f"UPDATE existing issue {key} {jira_summary} {description if verbose else ''}")
 				else:
 					jira.issue_update(key, fields_to_update)
 		# otherwise create a new issue
@@ -157,7 +210,7 @@ def main():
 				"issuetype": { "name": "Bug" }
 			}
 			if dry_run:
-				print(f"CREATE new issue {jira_summary} {finding['description']} {resources_to_text(finding['resources']) if verbose else ''}")
+				print(f"CREATE new issue {jira_summary} {description if verbose else ''}")
 			else:
 				jira.issue_create(fields_to_create)
 		
