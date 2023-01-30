cce6082d698e8c7bd14c7287169514c1ab545817
bhoff <bruce.hoff@sagebase.org>
2023-01-30 07:09:20 -0800

    updated call to Jira for atlassian-api-py package

--- security-hub-to-jira.py
+++ security-hub-to-jira.py
@@ -187,8 +187,7 @@ def main():
 	jira = Jira(
 	    url='https://sagebionetworks.jira.com/',
 	    username=os.environ['JIRA_USER_EMAIL_ADDRESS'],
-	    password=os.environ['JIRA_API_TOKEN'],
-	    cloud=True)
+	    password=os.environ['JIRA_API_TOKEN'])
 	    
 	issues = get_jira_sec_hub_issues(jira)
 	
